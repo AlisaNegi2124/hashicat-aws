3329852f8935878c43fa86b1b430694eb2ce798d
AlishaNegi2124 <126038428+AlisaNegi2124@users.noreply.github.com>
2023-02-22 13:59:22 +1000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. WE HAVE COMPLETED the FIRST RUN
+  Welcome to ${PREFIX}'s app. WE HAVE COMPLETED the FIRST RUN, Testing the second run
   <!-- END -->
 
   </div>
